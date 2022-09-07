007387e99efb8fbed48122c29ecbbe7a89f868c2
CHEREAL <mc.cherdak@mail.ru>
2022-09-07 21:52:12 +0300

    first time

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    dots: false,
+    margin: 48,
+    loop: true
+  });
+});
+
+/* video */
+
+const videoBtn = document.querySelector('#video-btn');
+const videoPicture = document.querySelector('.video__picture');
+const videoWrapper = document.querySelector('.video');
+const video = document.querySelector('#video-file');
+
+
+videoWrapper.addEventListener ('click', function () {
+  
+  if (video.paused) {
+    videoPicture.classList.add('none');
+videoWrapper.classList.remove('video-overlay');
+videoBtn.classList.add('none');
+    video.play();
+  }
+  else {
+    videoPicture.classList.add('none');
+    videoWrapper.classList.add('video-overlay');
+    videoBtn.classList.remove('none');
+  video.pause();
+  }
+})
